45d864dc6a842152ea0dc7e7e5663a0689dd7010
Matt Sroufe <matt.sroufe@powershop.co.nz>
2013-10-27 11:17:33 +1300

    refactor letter class

--- lib/scrabble/letter.rb
+++ lib/scrabble/letter.rb
@@ -1,38 +1,61 @@
 class Letter
 
-  COUNTS = {""=>2, "A"=>9, "B"=>2, "C"=>2, "D"=>4, "E"=>12, "F"=>2, "G"=>3, "H"=>2, "I"=>9, "J"=>1,
-           "K"=>1, "L"=>4, "M"=>2, "N"=>6, "O"=>8, "P"=>2, "Q"=>1, "R"=>6, "S"=>4, "T"=>6,
-           "U"=>4, "V"=>2, "W"=>2, "X"=>1, "Y"=>2, "Z"=>1}
-
-  SCORES = {""=>0,"A"=>1, "B"=>3, "C"=>3, "D"=>2, "E"=>1, "F"=>4, "G"=>2, "H"=>4, "I"=>1, "J"=>8,
-            "K"=>5, "L"=>1, "M"=>3, "N"=>1, "O"=>1, "P"=>3, "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
-            "U"=>1, "V"=>4, "W"=>4, "X"=>8, "Y"=>4, "Z"=>10}
+  def self.all
+    { ""  => {:score => 0,  :count => 2},
+      "A" => {:score => 1,  :count => 9},
+      "B" => {:score => 3,  :count => 2},
+      "C" => {:score => 3,  :count => 2},
+      "D" => {:score => 2,  :count => 4},
+      "E" => {:score => 1,  :count => 12},
+      "F" => {:score => 4,  :count => 2},
+      "G" => {:score => 2,  :count => 3},
+      "H" => {:score => 4,  :count => 2},
+      "I" => {:score => 1,  :count => 9},
+      "J" => {:score => 8,  :count => 1},
+      "K" => {:score => 5,  :count => 1},
+      "L" => {:score => 1,  :count => 4},
+      "M" => {:score => 3,  :count => 2},
+      "N" => {:score => 1,  :count => 6},
+      "O" => {:score => 1,  :count => 8},
+      "P" => {:score => 3,  :count => 2},
+      "Q" => {:score => 10, :count => 1},
+      "R" => {:score => 1,  :count => 6},
+      "S" => {:score => 1,  :count => 4},
+      "T" => {:score => 1,  :count => 6},
+      "U" => {:score => 1,  :count => 4},
+      "V" => {:score => 4,  :count => 2},
+      "W" => {:score => 4,  :count => 2},
+      "X" => {:score => 8,  :count => 1},
+      "Y" => {:score => 4,  :count => 2},
+      "Z" => {:score => 10, :count => 1} }
+  end
 
   MULTIPLIERS = {:single => 1, :double => 2, :triple => 3}
 
-  def self.all
-    @all ||= COUNTS.dup
+  def self.remaining
+    @remaining ||= all
   end
 
   def self.reset_letters
-    @all = COUNTS.dup
+    @remaining = all
   end
 
   def self.count
-    all.values.inject { |sum, number| sum + number }
+    remaining.values.map { |value| value[:count] }.inject do |sum, number|
+      sum + number
+    end
   end
 
   def self.remove(letter)
-    all[letter.upcase] -= 1
-    letter
+    remaining[letter.upcase][:count] -= 1
   end
 
   def self.select_random
-    remove(all.keys.sample)
+    remove(remaining.keys.sample)
   end
 
   def self.score(letter)
-    SCORES[letter.upcase]
+    all[letter.upcase][:score]
   end
 
   def self.multiplier(multiplier)
--- spec/scrabble/letter_spec.rb
+++ spec/scrabble/letter_spec.rb
@@ -1,6 +1,39 @@
 require 'spec_helper'
 
 describe Letter do
+  describe ".alls" do
+    it "returns a has of all the letters" do
+      expect(Letter.all).to eq(
+         { ""  => {:score => 0,  :count => 2},
+           "A" => {:score => 1,  :count => 9},
+           "B" => {:score => 3,  :count => 2},
+           "C" => {:score => 3,  :count => 2},
+           "D" => {:score => 2,  :count => 4},
+           "E" => {:score => 1,  :count => 12},
+           "F" => {:score => 4,  :count => 2},
+           "G" => {:score => 2,  :count => 3},
+           "H" => {:score => 4,  :count => 2},
+           "I" => {:score => 1,  :count => 9},
+           "J" => {:score => 8,  :count => 1},
+           "K" => {:score => 5,  :count => 1},
+           "L" => {:score => 1,  :count => 4},
+           "M" => {:score => 3,  :count => 2},
+           "N" => {:score => 1,  :count => 6},
+           "O" => {:score => 1,  :count => 8},
+           "P" => {:score => 3,  :count => 2},
+           "Q" => {:score => 10, :count => 1},
+           "R" => {:score => 1,  :count => 6},
+           "S" => {:score => 1,  :count => 4},
+           "T" => {:score => 1,  :count => 6},
+           "U" => {:score => 1,  :count => 4},
+           "V" => {:score => 4,  :count => 2},
+           "W" => {:score => 4,  :count => 2},
+           "X" => {:score => 8,  :count => 1},
+           "Y" => {:score => 4,  :count => 2},
+           "Z" => {:score => 10, :count => 1} }
+      )
+    end
+  end
   describe ".score" do
     it "scores a single letter" do
       expect(Letter.score("f")).to eq(4)
@@ -13,14 +46,24 @@ describe Letter do
     end
   end
 
+  describe ".reset_letters" do
+    it "resets Letter.remaing to the original letter values" do
+      expect(Letter.count).to eq(100)
+      Letter.remove("f")
+      expect(Letter.count).to eq(99)
+      Letter.reset_letters
+      expect(Letter.count).to eq(100)
+    end
+  end
+
   describe ".remove" do
     before do
-      Letter.stub(:all).and_return(Letter::COUNTS.dup)
+      Letter.reset_letters
     end
 
     it "removes the given letter from the letter bank" do
       Letter.remove("f")
-      expect(Letter.all["F"]).to eq(1)
+      expect(Letter.remaining["F"]).to eq({:score=>4, :count=>1})
     end
 
     it "lowers the letter count by 1" do
