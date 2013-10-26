8ac1c04ee76c3e16472e8f77e98dd9a0904d8f2c
Matt Sroufe <matt.sroufe@powershop.co.nz>
2013-10-27 12:29:29 +1300

    refactor letters

--- lib/scrabble/letter.rb
+++ lib/scrabble/letter.rb
@@ -1,33 +1,41 @@
 class Letter
 
+  LETTERS = { ""  => {:score => 0,  :count => 2},
+              "A" => {:score => 1,  :count => 9},
+              "B" => {:score => 3,  :count => 2},
+              "C" => {:score => 3,  :count => 2},
+              "D" => {:score => 2,  :count => 4},
+              "E" => {:score => 1,  :count => 12},
+              "F" => {:score => 4,  :count => 2},
+              "G" => {:score => 2,  :count => 3},
+              "H" => {:score => 4,  :count => 2},
+              "I" => {:score => 1,  :count => 9},
+              "J" => {:score => 8,  :count => 1},
+              "K" => {:score => 5,  :count => 1},
+              "L" => {:score => 1,  :count => 4},
+              "M" => {:score => 3,  :count => 2},
+              "N" => {:score => 1,  :count => 6},
+              "O" => {:score => 1,  :count => 8},
+              "P" => {:score => 3,  :count => 2},
+              "Q" => {:score => 10, :count => 1},
+              "R" => {:score => 1,  :count => 6},
+              "S" => {:score => 1,  :count => 4},
+              "T" => {:score => 1,  :count => 6},
+              "U" => {:score => 1,  :count => 4},
+              "V" => {:score => 4,  :count => 2},
+              "W" => {:score => 4,  :count => 2},
+              "X" => {:score => 8,  :count => 1},
+              "Y" => {:score => 4,  :count => 2},
+              "Z" => {:score => 10, :count => 1} }
+
   def self.all
-    { ""  => {:score => 0,  :count => 2},
-      "A" => {:score => 1,  :count => 9},
-      "B" => {:score => 3,  :count => 2},
-      "C" => {:score => 3,  :count => 2},
-      "D" => {:score => 2,  :count => 4},
-      "E" => {:score => 1,  :count => 12},
-      "F" => {:score => 4,  :count => 2},
-      "G" => {:score => 2,  :count => 3},
-      "H" => {:score => 4,  :count => 2},
-      "I" => {:score => 1,  :count => 9},
-      "J" => {:score => 8,  :count => 1},
-      "K" => {:score => 5,  :count => 1},
-      "L" => {:score => 1,  :count => 4},
-      "M" => {:score => 3,  :count => 2},
-      "N" => {:score => 1,  :count => 6},
-      "O" => {:score => 1,  :count => 8},
-      "P" => {:score => 3,  :count => 2},
-      "Q" => {:score => 10, :count => 1},
-      "R" => {:score => 1,  :count => 6},
-      "S" => {:score => 1,  :count => 4},
-      "T" => {:score => 1,  :count => 6},
-      "U" => {:score => 1,  :count => 4},
-      "V" => {:score => 4,  :count => 2},
-      "W" => {:score => 4,  :count => 2},
-      "X" => {:score => 8,  :count => 1},
-      "Y" => {:score => 4,  :count => 2},
-      "Z" => {:score => 10, :count => 1} }
+    letters = []
+    LETTERS.each do |k,v|
+      LETTERS[k][:count].times do
+        letters << k
+      end
+    end
+    letters
   end
 
   MULTIPLIERS = {:single => 1, :double => 2, :triple => 3}
@@ -40,22 +48,16 @@ class Letter
     @remaining = all
   end
 
-  def self.count
-    remaining.values.map { |value| value[:count] }.inject do |sum, number|
-      sum + number
-    end
-  end
-
   def self.remove(letter)
-    remaining[letter.upcase][:count] -= 1
+    @remaining.delete_at(remaining.index(letter.upcase))
   end
 
   def self.select_random
-    remove(remaining.keys.sample)
+    remaining.sample
   end
 
   def self.score(letter)
-    all[letter.upcase][:score]
+    LETTERS[letter.upcase][:score]
   end
 
   def self.multiplier(multiplier)
--- spec/scrabble/letter_spec.rb
+++ spec/scrabble/letter_spec.rb
@@ -1,58 +1,32 @@
 require 'spec_helper'
 
 describe Letter do
-  describe ".alls" do
+  describe ".all" do
     it "returns a has of all the letters" do
-      expect(Letter.all).to eq(
-         { ""  => {:score => 0,  :count => 2},
-           "A" => {:score => 1,  :count => 9},
-           "B" => {:score => 3,  :count => 2},
-           "C" => {:score => 3,  :count => 2},
-           "D" => {:score => 2,  :count => 4},
-           "E" => {:score => 1,  :count => 12},
-           "F" => {:score => 4,  :count => 2},
-           "G" => {:score => 2,  :count => 3},
-           "H" => {:score => 4,  :count => 2},
-           "I" => {:score => 1,  :count => 9},
-           "J" => {:score => 8,  :count => 1},
-           "K" => {:score => 5,  :count => 1},
-           "L" => {:score => 1,  :count => 4},
-           "M" => {:score => 3,  :count => 2},
-           "N" => {:score => 1,  :count => 6},
-           "O" => {:score => 1,  :count => 8},
-           "P" => {:score => 3,  :count => 2},
-           "Q" => {:score => 10, :count => 1},
-           "R" => {:score => 1,  :count => 6},
-           "S" => {:score => 1,  :count => 4},
-           "T" => {:score => 1,  :count => 6},
-           "U" => {:score => 1,  :count => 4},
-           "V" => {:score => 4,  :count => 2},
-           "W" => {:score => 4,  :count => 2},
-           "X" => {:score => 8,  :count => 1},
-           "Y" => {:score => 4,  :count => 2},
-           "Z" => {:score => 10, :count => 1} }
-      )
+      expect(Letter.all).to eq(["","","A","A","A","A","A","A","A","A","A","B","B","C","C","D","D","D","D","E","E","E","E","E","E","E","E","E","E","E","E","F","F","G","G","G","H","H","I","I","I","I","I","I","I","I","I","J","K","L","L","L","L","M","M","N","N","N","N","N","N","O","O","O","O","O","O","O","O","P","P","Q","R","R","R","R","R","R","S","S","S","S","T","T","T","T","T","T","U","U","U","U","V","V","W","W","X","Y","Y","Z"])
     end
   end
+
+  describe ".remaining" do
+    it "returns the remaining letters" do
+      expect(Letter.remaining).to eq(["","","A","A","A","A","A","A","A","A","A","B","B","C","C","D","D","D","D","E","E","E","E","E","E","E","E","E","E","E","E","F","F","G","G","G","H","H","I","I","I","I","I","I","I","I","I","J","K","L","L","L","L","M","M","N","N","N","N","N","N","O","O","O","O","O","O","O","O","P","P","Q","R","R","R","R","R","R","S","S","S","S","T","T","T","T","T","T","U","U","U","U","V","V","W","W","X","Y","Y","Z"])
+      expect(Letter.remaining.count).to eq(100)
+    end
+  end
+
   describe ".score" do
     it "scores a single letter" do
       expect(Letter.score("f")).to eq(4)
     end
   end
 
-  describe ".count" do
-    it "returns the number of letters left" do
-      expect(Letter.count).to eq(100)
-    end
-  end
-
   describe ".reset_letters" do
     it "resets Letter.remaing to the original letter values" do
-      expect(Letter.count).to eq(100)
+      expect(Letter.remaining.count).to eq(100)
       Letter.remove("f")
-      expect(Letter.count).to eq(99)
+      expect(Letter.remaining.count).to eq(99)
       Letter.reset_letters
-      expect(Letter.count).to eq(100)
+      expect(Letter.remaining.count).to eq(100)
     end
   end
 
@@ -63,12 +37,12 @@ describe Letter do
 
     it "removes the given letter from the letter bank" do
       Letter.remove("f")
-      expect(Letter.remaining["F"]).to eq({:score=>4, :count=>1})
+      expect(Letter.remaining.count).to eq(99)
     end
 
     it "lowers the letter count by 1" do
       Letter.remove("f")
-      expect(Letter.count).to eq(99)
+      expect(Letter.remaining.count).to eq(99)
     end
   end
 
